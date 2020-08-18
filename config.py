80fde556db3fd189d1545fec44355042535d56e7
Annamalai Narayanan <annamalai@Annamalais-MacBook-Air.local>
2020-08-18 13:45:51 +0800

    config changes for class weights in categorical cross ent loss

--- config.py
+++ config.py
@@ -2,19 +2,19 @@ import os
 import psutil
 import torch
 
-data_folder = '/home/anna/toxic_comment_classification/data'
+data_folder = './data'
 train_fname = data_folder + '/train.csv'
 test_fname = data_folder + '/test.csv'
 
 MAX_SEQ_LEN = 512
 NUM_EPOCHS = 20
-BATCH_SIZE = 100
+BATCH_SIZE = 40
 LR = 3e-4
 NUM_CPU_WORKERS = psutil.cpu_count()
-PRINT_EVERY = 100
+PRINT_EVERY = 10
 BERT_LAYER_FREEZE = True
 
-SAMPLE_RATIO = 0.2
+SAMPLE_RATIO = 0.3
 VALIDATION_SET_RATIO = 0.2
 
 MULTIGPU = True if torch.cuda.device_count() > 1 else False
