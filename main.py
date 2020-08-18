0302beff21fb1e2b38569d7b471aa2b1c33024ce
Annamalai Narayanan <annamalai@Annamalais-MacBook-Air.local>
2020-08-18 13:45:11 +0800

    added support for class weights in categorical cross ent loss

--- main.py
+++ main.py
@@ -35,7 +35,6 @@ def test(net, test_loader, device='cpu'):
     net.eval()
     with torch.no_grad():
         for (seq, attn_masks, labels) in tqdm(test_loader):
-
             seq, attn_masks = seq.cuda(device), attn_masks.cuda(device)
             logits = net(seq, attn_masks)
             auc_for_all_classes = get_auc_from_logits(logits, labels)
@@ -63,23 +62,25 @@ def train_model(net, criterion, optimizer, scheduler, train_loader, test_loader=
             logits = net(seq, attn_mask)
 
             # Computing loss
-            loss = 0.
+            loss_all_classes = 0.
             for class_id in range(6):
                 pred = logits[class_id]
                 true = labels[:,class_id]
-                loss += criterion(pred, true.long())
-            e_loss.append(loss.item())
+                loss_for_this_class = criterion[class_id](pred, true.long())
+                loss_all_classes += loss_for_this_class
+            e_loss.append(loss_all_classes.item())
 
-            # Backpropagating the gradients
-            loss.backward()
+            # Backpropagating the gradients for losses on all classes
+            loss_all_classes.backward()
 
             # Optimization step
             optimizer.step()
             scheduler.step()
 
             if (batch_num + 1) % print_every == 0:
-                acc = get_auc_from_logits(logits, labels)
-                print(f"batch {batch_num+1} of epoch {e} complete. Loss : {loss.item()} AUC : {acc}")
+                auc = get_auc_from_logits(logits, labels.cpu().numpy())
+                print(f"batch {batch_num+1} of epoch {e} complete. Loss : {loss_all_classes.item()} "
+                      f"AUC : {auc.round(2)}")
 
         t = time.perf_counter() - t0
         e_loss = np.array(e_loss).mean()
@@ -88,6 +89,24 @@ def train_model(net, criterion, optimizer, scheduler, train_loader, test_loader=
             test_acc = test(net, test_loader, device=device)
             print(f'After epoch: {e}, test AUC: {test_acc.round(2)}')
 
+
+def compute_class_weight_balanced(y):
+    n_samples = len(y)
+    n_classes = 2
+    w = n_samples / (n_classes * np.bincount(y))
+    return w
+
+
+def get_class_weigts(train_df, max_class_weight=50.):
+    class_weights_dict = {}
+    label_cols = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+    train_df = train_df[label_cols]
+    labels = np.array(train_df, dtype=int)
+    for col_index in range(labels.shape[1]):
+        w = compute_class_weight_balanced(labels[:,col_index])
+        w = w.clip(max = max_class_weight)
+        class_weights_dict[col_index] = w
+    return class_weights_dict
 
 
 def main():
@@ -116,7 +135,9 @@ def main():
     bert_model.cuda()
 
     #loss function
-    criterion = nn.CrossEntropyLoss()
+    class_weights_dict = get_class_weigts(train_df)
+    class_weights_dict = {c: torch.tensor(w).to(device).float() for c, w in class_weights_dict.items()}
+    criterion = [nn.CrossEntropyLoss(weight=class_weights_dict[c]) for c in sorted(class_weights_dict.keys())]
 
     #optimizer and scheduler
     # optimizer = optim.Adam(bert_model.parameters(), lr=config.LR)
@@ -149,8 +170,8 @@ def main():
 
     print(f"created BERT model for finetuning: {bert_model}")
 
-    aucs_for_samples = test(bert_model, test_loader, device=device)
-    print(f'AUC before training: {aucs_for_samples.round(2)}')
+    # aucs_for_samples = test(bert_model, test_loader, device=device)
+    # print(f'AUC before training: {aucs_for_samples.round(2)}')
     train_model(bert_model, criterion, optimizer, scheduler, train_loader, test_loader,
                 print_every=config.PRINT_EVERY, n_epochs=config.NUM_EPOCHS, device=device)
 
