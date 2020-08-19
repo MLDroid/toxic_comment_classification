6dd909f37aef60f239de769586e710eb0517c371
Annamalai Narayanan <annamalai@Annamalais-MacBook-Air.local>
2020-08-19 16:12:08 +0800

    adding support for saving train and valid dfs and load test set

--- dataset.py
+++ dataset.py
@@ -5,10 +5,8 @@ import pandas as pd
 import numpy as np
 from tqdm import tqdm
 
-import config
 
-
-def get_train_valid_df(dataset_fname, sample_ratio=None, valid_ratio=0.2):
+def get_train_valid_df(dataset_fname, sample_ratio=None, valid_ratio=0.2, save_dfs=True):
     df = pd.read_csv(dataset_fname)
     if sample_ratio:
         df = df.sample(frac=sample_ratio)
@@ -16,6 +14,10 @@ def get_train_valid_df(dataset_fname, sample_ratio=None, valid_ratio=0.2):
 
     train_df = df.sample(frac=1-valid_ratio)
     valid_df = df[~df.index.isin(train_df.index)]
+
+    if save_dfs:
+        train_df.to_csv('train_df_.csv',index=False)
+        valid_df.to_csv('valid_df_.csv',index=False)
 
     return train_df, valid_df
 
@@ -56,15 +58,56 @@ class dataset(Dataset):
 
         # Obtaining the attention mask i.e a tensor containing 1s for no padded tokens and 0s for padded ones
         attn_mask = (tokens_ids_tensor != 0).long()
-
         return tokens_ids_tensor, attn_mask
+
 
     def __len__(self):
         return len(self.labels)
+
 
     def __getitem__(self, index):
         #Selecting the sentence and label at the specified index in the data frame
         token_ids,attn_mask = self.sent_token_ids_attn_masks[index] #list index
         label = self.labels[index] #array index
-
         return token_ids, attn_mask, label
+
+
+
+class test_dataset(Dataset):
+    def __init__(self, df, max_len):
+        # Initialize the BERT tokenizer
+        # self.tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
+        self.tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+        self.max_len = max_len
+
+        self.sent_token_ids_attn_masks = [self._get_token_ids_attn_mask(s) for s in tqdm(df.comment_text)]
+
+        print(f'Loaded X_test shape: {len(self.sent_token_ids_attn_masks)}')
+
+
+    def _get_token_ids_attn_mask(self, sentence):
+        sentence = sentence.lower().strip()
+        tokens = self.tokenizer.tokenize(sentence)  # Tokenize the sentence
+        tokens = ['[CLS]'] + tokens + ['[SEP]']  # Insering the CLS and SEP token in the beginning and end of the sentence
+        if len(tokens) < self.max_len:
+            tokens = tokens + ['[PAD]' for _ in range(self.max_len - len(tokens))]  # Padding sentences
+        else:
+            tokens = tokens[:self.max_len - 1] + ['[SEP]']  # Pruning the list to be of specified max length
+
+        tokens_ids = self.tokenizer.convert_tokens_to_ids(
+            tokens)  # Obtaining the indices of the tokens in the BERT Vocabulary
+        tokens_ids_tensor = torch.tensor(tokens_ids)  # Converting the list to a pytorch tensor
+
+        # Obtaining the attention mask i.e a tensor containing 1s for no padded tokens and 0s for padded ones
+        attn_mask = (tokens_ids_tensor != 0).long()
+        return tokens_ids_tensor, attn_mask
+
+
+    def __len__(self):
+        return len(self.sent_token_ids_attn_masks)
+
+
+    def __getitem__(self, index):
+        #Selecting the sentence at the specified index in the data frame
+        token_ids,attn_mask = self.sent_token_ids_attn_masks[index] #list index
+        return token_ids, attn_mask
