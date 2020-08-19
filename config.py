48e6db8e39697dc91c18756675126b0759898ea5
Annamalai Narayanan <annamalai.hive@gmail.com>
2020-08-20 00:11:14 +0800

    Added comments

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
-import os
 import psutil
 import torch
 import sys
-
 import os
+
 os.environ["TOKENIZERS_PARALLELISM"] = "true"
 
 MODEL_NAME =  sys.argv[1]
@@ -14,21 +13,20 @@ data_folder = './data'
 train_fname = data_folder + '/train.csv'
 test_fname = data_folder + '/test.csv'
 
-MAX_SEQ_LEN = 512
-if 'longformer' in MODEL_NAME:
-    MAX_SEQ_LEN = 4096
+MAX_SEQ_LEN = 4096 if 'longformer' in MODEL_NAME else 512
 NUM_EPOCHS = 10
-BATCH_SIZE = 200
+BATCH_SIZE = 30
 LR = 3e-4
 NUM_CPU_WORKERS = psutil.cpu_count()
 PRINT_EVERY = 100
 BERT_LAYER_FREEZE = True
 
-SAMPLE_RATIO = None
-VALIDATION_SET_RATIO = 0.3
+SAMPLE_RATIO = None #load only this % of the train.csv DF
+VALIDATION_SET_RATIO = 0.3 #validation set extracted from training set
 
-MULTIGPU = True if torch.cuda.device_count() > 1 else False
+MULTIGPU = True if torch.cuda.device_count() > 1 else False #when using xlarge vs 16x large AWS m/c
 
+#these 2 are not used yet
 TRAINED_MODEL_FNAME_PREFIX = MODEL_NAME.upper()+'_toxic_comment_model'
 TRAINED_MODEL_FNAME = None
 
