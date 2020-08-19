3fcbade83097e4c7ac5f85797624cce7df81080e
Annamalai Narayanan <annamalai.hive@gmail.com>
2020-08-20 00:11:44 +0800

    Clean up and added comments

--- dataset.py
+++ dataset.py
@@ -12,12 +12,9 @@ import config
 
 def get_train_valid_df(dataset_fname, sample_ratio=None, valid_ratio=0.2, save_dfs=True):
     df = pd.read_csv(dataset_fname)
-    if sample_ratio:
-        df = df.sample(frac=sample_ratio)
+    #check if the whole DF is needed
+    if sample_ratio: df = df.sample(frac=sample_ratio)
     print(f'Loaded dataframe of shape: {df.shape} from {dataset_fname}')
-
-    # train_df = df.sample(frac=1-valid_ratio)
-    # valid_df = df[~df.index.isin(train_df.index)]
 
     train_df, valid_df = train_test_split(df, random_state=42, test_size=valid_ratio, shuffle=True)
 
@@ -34,8 +31,8 @@ class dataset(Dataset):
     def __init__(self, df, max_len):
         self.model_name = config.MODEL_NAME
         self.max_len = max_len
-        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name, use_fast=True)
 
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name, use_fast=True)
         print(f'Set max seq. len: {self.max_len} for tokenizer: {self.tokenizer}')
 
         self.sent_token_ids_attn_masks = [self._get_token_ids_attn_mask(s) for s in tqdm(df.comment_text)]
@@ -53,28 +50,17 @@ class dataset(Dataset):
 
     def _get_token_ids_attn_mask(self, sentence):
         sentence = sentence.lower().strip()
-        sentence = ' '.join(sentence.split())
+        sentence = ' '.join(sentence.split()) #make sure unwanted spaces are removed
 
-        # tokens = self.tokenizer.tokenize(sentence)  # Tokenize the sentence
-        # tokens = ['[CLS]'] + tokens + ['[SEP]']  # Insering the CLS and SEP token in the beginning and end of the sentence
-        # if len(tokens) < self.max_len:
-        #     tokens = tokens + ['[PAD]' for _ in range(self.max_len - len(tokens))]  # Padding sentences
-        # else:
-        #     tokens = tokens[:self.max_len - 1] + ['[SEP]']  # Pruning the list to be of specified max length
-        #
-        # tokens_ids = self.tokenizer.convert_tokens_to_ids(
-        #     tokens)  # Obtaining the indices of the tokens in the BERT Vocabulary
-        # tokens_ids_tensor = torch.tensor(tokens_ids)  # Converting the list to a pytorch tensor
-        # # Obtaining the attention mask i.e a tensor containing 1s for no padded tokens and 0s for padded ones
-        # attn_mask = (tokens_ids_tensor != 0).long()
-        # return tokens_ids_tensor, attn_mask
-
+        #encode_plus is better than calling tokenizer.tokenize and get the IDs later - ref:Abisek Thakur youtube video
         inputs = self.tokenizer.encode_plus(sentence, None,
                                             add_special_tokens=True,
                                             max_length=self.max_len,
                                             pad_to_max_length=True,
                                             truncation=True
                                             )
+
+        #need to convert them as tensors
         tokens_ids_tensor = torch.tensor(inputs["input_ids"], dtype=torch.long)
         attn_mask = torch.tensor(inputs["attention_mask"], dtype=torch.long)
         return tokens_ids_tensor, attn_mask
