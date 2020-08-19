9915d4018e4401ed8561d3b95957fec4acddb8c4
Annamalai Narayanan <annamalai@Annamalais-MacBook-Air.local>
2020-08-19 16:16:06 +0800

    changed AUC compuatation manner

--- config.py
+++ config.py
@@ -7,15 +7,20 @@ train_fname = data_folder + '/train.csv'
 test_fname = data_folder + '/test.csv'
 
 MAX_SEQ_LEN = 512
-NUM_EPOCHS = 20
-BATCH_SIZE = 40
+NUM_EPOCHS = 10
+BATCH_SIZE = 200
 LR = 3e-4
 NUM_CPU_WORKERS = psutil.cpu_count()
-PRINT_EVERY = 10
+PRINT_EVERY = 100
 BERT_LAYER_FREEZE = True
 
-SAMPLE_RATIO = 0.3
+SAMPLE_RATIO = None
 VALIDATION_SET_RATIO = 0.2
 
 MULTIGPU = True if torch.cuda.device_count() > 1 else False
+
+TRAINED_MODEL_FNAME_PREFIX = 'distilbert_toxic_comment'
+TRAINED_MODEL_FNAME = None
+
+
 
